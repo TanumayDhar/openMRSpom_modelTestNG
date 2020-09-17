e6066c8a46c531987a2ada0526668a0b9af70d6d
Tanumay Dhar <tanna.dhar@gmail.com>
2020-09-17 21:31:05 +0530

    test from eclpise

--- src/test/java/com/openMRS/TestNGScripts/LoginTest.java
+++ src/test/java/com/openMRS/TestNGScripts/LoginTest.java
@@ -1,12 +1,9 @@
 package com.openMRS.TestNGScripts;
 
-import java.io.File;
+
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
